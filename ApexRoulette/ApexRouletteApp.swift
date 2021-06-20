fa39ce8006a517b85b4f4c7285648b27964e7fde
nboyers <frenttv@gmail.com>
2021-06-20 13:06:44 -0400

    Initial Commit

---
+++ ApexRoulette/ApexRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexRouletteApp.swift
+//  ApexRoulette
+//
+//  Created by Noah Boyers on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ApexRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
